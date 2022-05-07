31ad431b53505122c0805b7f216be6e802256508
VlasDaniela <95177537+VlasDaniela@users.noreply.github.com>
2022-05-07 14:52:55 +0300

    lab3

---
+++ resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by lab3.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_LAB3_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_A                           1001
+#define IDC_OPERATIE1                   1003
+#define IDC_RADIO3                      1005
+#define IDC_RADIO4                      1006
+#define IDC_RADIO5                      1007
+#define IDC_RADIO6                      1008
+#define IDC_RADIO7                      1009
+#define IDC_RADIO8                      1010
+#define IDC_STATIC1                     1011
+#define IDC_EDIT2                       1014
+#define IDC_B                           1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
